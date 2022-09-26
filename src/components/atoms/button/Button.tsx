2198a92a3e57fd007c6825e77a38051dae091a48
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2022-09-26 12:08:04 +0100

    button size change

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 export const Button = ({ color = '#c21c1c', size, label, ...props }: ButtonProps) => {
 	return (
 		<button
-			className={`text-2xl font-mono 
+			className={`text-3xl font-mono 
 			${size === 'sm' ? 'py-1 px-2' : ''}
 			${size === 'md' ? 'py-2 px-4' : ''}
 			${size === 'lg' ? 'py-4 px-6' : ''}
